c437cadfce24d59f5ac2957fa3c5297ddd2cdcf7
Siva kumar <99171876+Siva1kumar@users.noreply.github.com>
2022-02-21 13:48:56 +0530

    Add files via upload

---
+++ Models/entities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvcdress.Models
+{
+    
+    
+        public partial class Mall
+        {
+            public int Mall_id { get; set; }
+            public string Mall_name { get; set; }
+            public string dress_type { get; set; }
+            public decimal dress_price { get; set; }
+            public string dress_size { get; set; }
+        }
+    
+}
